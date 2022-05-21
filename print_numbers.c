d9a9ee9cf110d9d1deddf1b0d4684598548d1d1e
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-05-21 14:11:28 +0000

    print_numbers file prints numbers from -infinity to infinity

---
+++ print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using the oogway function onlu.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		oogway('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	oogway((num % 10) + '0');
+}
